4cdddebc1e30c957eb42262fc006fe3bbff0b888
Rajudas575 <rajudas575@gmail.com>
2024-12-19 20:44:35 +0530

    Added tested script from local

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [inputName, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  
   return (
     <div>
         {/* Do not remove the main div */}
+      <label>Enter your name</label>
+      <input type="text" value={inputName} onChange={handleChange} />
+      {inputName && <p>Hello, {inputName}!</p>}
     </div>
   )
 }
